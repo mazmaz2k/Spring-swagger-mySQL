47a58a379a1ce863c10e5e50f9f04532384977c7
mazmaz2k <mazmaz2k@gmail.com>
2018-12-22 16:12:03 +0200

    first commit

---
+++ src/main/java/com/example/omri/SpringSwaggerMysqlApplication.java
@@ -0,0 +1,14 @@
+package com.example.omri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSwaggerMysqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSwaggerMysqlApplication.class, args);
+	}
+
+}
+
